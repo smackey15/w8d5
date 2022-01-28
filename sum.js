cf7730594646ec343df724c4b63ebf32e2c74e6a
sean <midruss@yahoo.com>
2022-01-28 12:16:58 -0500

    Finished sum

--- sum.js
+++ sum.js
@@ -1,10 +1,27 @@
-function sum() {
-  let sum = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
-  }
+// function sum() {
+//   let sum = 0;
+//   for (let i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
 
-  return sum;
+//   return sum;
+// }
+
+// function sum(...args) {
+//   let sum = 0
+//   for (let i = 0; i < args.length; i++) {
+//     sum += args[i]
+//   }
+//   return sum
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
+
+function sum(...args) {
+  let sum = 0
+  args.forEach(el => {
+    sum += el
+  })
+  return sum
 }
-
 console.log(sum(1, 2, 3, 4, 5));
